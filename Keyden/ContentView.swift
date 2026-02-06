981835f73a411188bde76a6d1b17faca5cbe091e
cookabc <chuangcius@gmail.com>
2026-02-06 18:41:49 +0800

    Initial Commit

---
+++ Keyden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Keyden
+//
+//  Created by 宋许刚 on 2026/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Keyden/KeydenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeydenApp.swift
+//  Keyden
+//
+//  Created by 宋许刚 on 2026/2/6.
+//
+
+import SwiftUI
+
+@main
+struct KeydenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
